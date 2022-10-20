a52dc1828c91691b5b614d4f0a37f827877edaeb
ALBMOR <ALBMOR@BARL202279>
2022-10-20 11:59:16 +0200

    Add project files.

---
+++ PassGen/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace PassGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("DNV Display Medium", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Password Generator";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox1.Location = new System.Drawing.Point(28, 141);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 22);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "8 Characters";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox2.Location = new System.Drawing.Point(28, 169);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(125, 22);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "12 Characters";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.checkBox3.Location = new System.Drawing.Point(28, 198);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(126, 22);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "16 Characters";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(331, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(25, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(313, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("DNV Display Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(7, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(505, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "_____________________________________________";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.checkedListBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "English",
+            "Spanish",
+            "Catalan",
+            "Portuguese"});
+            this.checkedListBox1.Location = new System.Drawing.Point(28, 281);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(458, 84);
+            this.checkedListBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(25, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select lenguage for Email template:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label6.Location = new System.Drawing.Point(25, 385);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Email to send credentials:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(230, 382);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(256, 27);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "@dnv.com";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 29);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Create document";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("DNV Display Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(7, 468);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(505, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "_____________________________________________";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label7.Location = new System.Drawing.Point(25, 419);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 18);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Secure code";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("DNV Display Medium", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.Info;
+            this.label8.Location = new System.Drawing.Point(186, 223);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(300, 14);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "By default it will generate a 12 characters password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(28, 441);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 25);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 514);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 26);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Clear Form";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label9.Location = new System.Drawing.Point(28, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 18);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "User:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(331, 25);
+            this.textBox3.TabIndex = 23;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(313, 454);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(173, 29);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Create drafts";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(526, 565);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("DNV Display Medium", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Password Generator ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ PassGen/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Interop.Word;
+using System.Collections.Generic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PassGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int len = 6;
+            const string validchar = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWZ1234567890!@,.?";
+            StringBuilder result = new StringBuilder();
+            Random rand = new Random();
+            while (0 < len--)
+            { result.Append(validchar[rand.Next(validchar.Length)]); }
+            textBox2.Text = result.ToString();
+            string CreatedPass;
+            string SecureCode;
+            string Usr;
+            string besendto;
+            CreatedPass = textBox1.Text;
+            SecureCode = textBox2.Text;
+            Usr = textBox3.Text;
+            besendto = richTextBox1.Text;
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                PassGen(8);
+            }
+            else if (checkBox2.Checked)
+            {
+                PassGen(12);
+            }
+            else if (checkBox3.Checked)
+            {
+                PassGen(16);
+            }
+            else { PassGen(12); }
+        }
+        public void PassGen(int len)
+        {
+            const string validchar = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWZ1234567890!@,.?";
+            StringBuilder result = new StringBuilder();
+            Random rand = new Random();
+            while (0 < len--)
+            { result.Append(validchar[rand.Next(validchar.Length)]); }
+            textBox1.Text = result.ToString();
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int len = 6;
+            const string validchar = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWZ1234567890!@,.?";
+            StringBuilder result = new StringBuilder();
+            Random rand = new Random();
+            while (0 < len--)
+            { result.Append(validchar[rand.Next(validchar.Length)]); }
+            textBox2.Text = result.ToString();
+            richTextBox1.Text = ("@dnv.com");
+            textBox3.Clear();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateDocument();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CreateMailItem1();
+            CreateMailItem2();
+        }
+        //Create document method  
+        private void CreateDocument()
+        {
+            try
+            {
+                string CreatedPass;
+                string SecureCode;
+                string Usr;
+                string besendto;
+                CreatedPass = textBox1.Text;
+                SecureCode = textBox2.Text;
+                Usr = textBox3.Text;
+                besendto = richTextBox1.Text;
+                //Create an instance for word app  
+                Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+
+                //Set animation status for word application  
+                winword.ShowAnimation = false;
+
+                //Set status for word application is to be visible or not.  
+                winword.Visible = false;
+
+                //Create a missing variable for missing value  
+                object missing = System.Reflection.Missing.Value;
+
+                //Create a new document  
+                Microsoft.Office.Interop.Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+                //Add header into the document  
+                //foreach (Microsoft.Office.Interop.Word.Section section in document.Sections)
+                // {
+                //Get the header range and add the header details.  
+                //  Microsoft.Office.Interop.Word.Range headerRange = section.Headers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                //headerRange.Fields.Add(headerRange, Microsoft.Office.Interop.Word.WdFieldType.wdFieldPage);
+                //  headerRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                //headerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdBlue;
+                //headerRange.Font.Size = 10;
+                // headerRange.Text = "Header text goes here";
+                //  }
+
+                //Add the footers into the document  
+                //foreach (Microsoft.Office.Interop.Word.Section wordSection in document.Sections)
+                //{
+                //Get the footer range and add the footer details.  
+                //Microsoft.Office.Interop.Word.Range footerRange = wordSection.Footers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                //footerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdDarkRed;
+                //footerRange.Font.Size = 10;
+                //footerRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                //footerRange.Text = "Footer text goes here";
+                //}
+
+                //adding text to document  
+                document.Content.SetRange(0, 0);
+                document.Content.Text = "Your new password for the account " + Usr + " is " + CreatedPass;
+
+                //Add paragraph with Heading 1 style  
+                // Microsoft.Office.Interop.Word.Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
+                //object styleHeading1 = "Heading 1";
+                //para1.Range.set_Style(ref styleHeading1);
+                //para1.Range.Text = "Para 1 text";
+                //para1.Range.InsertParagraphAfter();
+
+                //Add paragraph with Heading 2 style  
+                //Microsoft.Office.Interop.Word.Paragraph para2 = document.Content.Paragraphs.Add(ref missing);
+                //object styleHeading2 = "Heading 2";
+                //para2.Range.set_Style(ref styleHeading2);
+                //para2.Range.Text = "Para 2 text";
+                //para2.Range.InsertParagraphAfter();
+
+                //Create a 5X5 table and insert some dummy record  
+                //Table firstTable = document.Tables.Add(para1.Range, 5, 5, ref missing, ref missing);
+
+                // firstTable.Borders.Enable = 1;
+                //foreach (Row row in firstTable.Rows)
+                //{
+                //foreach (Cell cell in row.Cells)
+                //{
+                //Header row  
+                //                if (cell.RowIndex == 1)
+                //{
+                //cell.Range.Text = "Column " + cell.ColumnIndex.ToString();
+                //cell.Range.Font.Bold = 1;
+                //other format properties goes here  
+                //cell.Range.Font.Name = "verdana";
+                //cell.Range.Font.Size = 10;
+                //cell.Range.Font.ColorIndex = WdColorIndex.wdGray25;                              
+                //cell.Shading.BackgroundPatternColor = WdColor.wdColorGray25;
+                //Center alignment for the Header cells  
+                //cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                //cell.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                //
+                //}
+                //Data row  
+                //      else
+                //{
+                //cell.Range.Text = (cell.RowIndex - 2 + cell.ColumnIndex).ToString();
+                //}
+                //}
+                //}
+
+                //Save the document
+                object filename = @"c:\Temp\Credentials.docx";
+                object Password = SecureCode;
+                document.Password = SecureCode;
+                document.SaveAs(ref filename);
+                document.Close(ref missing, ref missing, ref missing);
+                document = null;
+                winword.Quit(ref missing, ref missing, ref missing);
+                winword = null;
+                MessageBox.Show("Document created successfully !");
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+   private void CreateMailItem2()
+    {
+        string CreatedPass;
+        string SecureCode;
+        string Usr;
+        string besendto;
+        CreatedPass = textBox1.Text;
+        SecureCode = textBox2.Text;
+        Usr = textBox3.Text;
+        besendto = richTextBox1.Text;
+
+        Microsoft.Office.Interop.Outlook.Application app = new Microsoft.Office.Interop.Outlook.Application();
+        MailItem item = app.CreateItem((OlItemType.olMailItem));
+        item.BodyFormat = OlBodyFormat.olFormatHTML;
+        item.To = besendto;
+        item.Body = "The code for the Word file is: " + SecureCode;
+        item.Subject = "New Credentials - 2";
+        item.Display(false);
+        item.Save();
+
+    }
+}
+
