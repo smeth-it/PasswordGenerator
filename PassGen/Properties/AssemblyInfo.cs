848c4dac602cc70c74d7bf09c9e6329e649160bb
Albert Moreno <Albert.Moreno@dnv.com>
2022-11-11 16:48:48 +0100

    updates v0.2.1

--- PassGen/Properties/AssemblyInfo.cs
+++ PassGen/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c4d364f0-b5b4-4399-ae51-162b2b3e6e6e")]
